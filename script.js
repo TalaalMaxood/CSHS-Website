806d20f03804146e84dc02d736ce71409f8b14a6
TalaalMaxood <102316429+TalaalMaxood@users.noreply.github.com>
2022-04-12 12:59:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks");
+
+function hideMenu(){
+    navLinks.style.visibility = "hidden";
+}
+
+function showMenu(){
+    navLinks.style.visibility = "visible";
+}
+
